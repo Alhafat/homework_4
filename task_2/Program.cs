af199eb520a5ba76537abb9f40964ebdf44dffcb
Alhafat <vladimir-lukin@inbox.ru>
2023-01-27 14:59:00 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+
+int Summ(int number)
+{   
+    int summ=0;
+    while (number>0)
+    {
+        summ=summ+number%10;
+        number=number/10;
+    }
+    return (summ);
+}
+
+Console.Write("Введите число: ");
+int number=int.Parse(Console.ReadLine()!);
+int summ=Summ(number);
+Console.WriteLine($"Сумма цифр числа {number} = {summ}");
