eac5d8cf9484624f89745a8b6db25ae61ff01b11
Alhafat <vladimir-lukin@inbox.ru>
2023-01-27 23:53:08 +0300

    task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -8,38 +8,10 @@ Console.Clear();
 
 // // Вариант 1
 
-// int NumberPowB(int numberA)
-// {   
-//     int result=1;
-//     int numberB=int.Parse(Console.ReadKey().KeyChar.ToString());
-//     if (numberB>0)
-//     {
-//         for (int i=0; i<numberB; i++)
-//         {
-//             result*=numberA;
-//         }
-//     }
-//     else if (numberB==0) result=1;
-//     // else if (B<0) 
-//     // {
-//     //     result=number*(1/Math.Abs(B));
-//     // }
-//     return (result);
-// }
-
-// Console.Write("Введите число: ");
-// int numberA=int.Parse(Console.ReadLine()!);
-// Console.Write($"Число {numberA} в степени ");
-// int result=NumberPowB(numberA);
-// Console.WriteLine($" = {result} ");
-
-
-// // Вариант 2
-
-Console.Clear();
-int NumberAToPownumberB(int numberA, int numberB)
+int NumberPowB(int numberA)
 {   
     int result=1;
+    int numberB=int.Parse(Console.ReadKey().KeyChar.ToString());
     if (numberB>0)
     {
         for (int i=0; i<numberB; i++)
@@ -57,7 +29,35 @@ int NumberAToPownumberB(int numberA, int numberB)
 
 Console.Write("Введите число: ");
 int numberA=int.Parse(Console.ReadLine()!);
-Console.Write("Введите степень числа: ");
-int numberB=int.Parse(Console.ReadLine()!);
-int result=NumberAToPownumberB(numberA, numberB);
-Console.Write($"Число {numberA} в степени {numberB} = {result}");
+Console.Write($"Число {numberA} в степени ");
+int result=NumberPowB(numberA);
+Console.WriteLine($" = {result} ");
+
+
+// // Вариант 2
+
+// Console.Clear();
+// int NumberAToPownumberB(int numberA, int numberB)
+// {   
+//     int result=1;
+//     if (numberB>0)
+//     {
+//         for (int i=0; i<numberB; i++)
+//         {
+//             result*=numberA;
+//         }
+//     }
+//     else if (numberB==0) result=1;
+//     // else if (B<0) 
+//     // {
+//     //     result=number*(1/Math.Abs(B));
+//     // }
+//     return (result);
+// }
+
+// Console.Write("Введите число: ");
+// int numberA=int.Parse(Console.ReadLine()!);
+// Console.Write("Введите степень числа: ");
+// int numberB=int.Parse(Console.ReadLine()!);
+// int result=NumberAToPownumberB(numberA, numberB);
+// Console.Write($"Число {numberA} в степени {numberB} = {result}");
